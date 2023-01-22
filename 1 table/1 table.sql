0e77826ff8519b92c6d32f43b47121d56b60309f
shavymittelman <113139414+shavymittelman@users.noreply.github.com>
2023-01-22 13:08:09 -0500

    Update 1 table.sql
    add planet to the body table

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -2,6 +2,7 @@ use humanbodyDB
 go 
 drop table if exists body
 create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null,
-num int null
+  planet varchar(50) not null,
+  bodypart varchar(100) not null,
+  num int null
 )
